84874b4dbd3ae6e4f7c36d9d4d18cd9d68f89858
asnakamu <amysnak@gmail.com>
2017-02-10 15:08:36 -0800

    slider listener added

--- src/gameUI/GameController.java
+++ src/gameUI/GameController.java
@@ -1,21 +1,28 @@
 package gameUI;
 
-import java.awt.event.ActionEvent;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
-import javafx.event.EventHandler;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.ContextMenu;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.GridPane;
+
 import javafx.stage.Stage;
 
 
@@ -57,9 +64,23 @@ public class GameController implements Initializable {
     @FXML
     private Menu saveMenu;
     
+    // AUDIO WINDOW INJECTIONS
+    @FXML
+    private Slider volumeSlider;
+
+    @FXML
+    private Label volumeLabel;
+
+    @FXML
+    private AnchorPane audioWindowBasePane;
+
+    @FXML
+    private GridPane audioGrid;
+
+    
     @Override // This method is called by the FXMLLoader when initialization is complete
     public void initialize(URL fxmlFileLocation, ResourceBundle resources) {
-
+        
     }
     
     @FXML
@@ -68,12 +89,29 @@ public class GameController implements Initializable {
             FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("AudioWindow.fxml"));
                     Parent root1 = (Parent) fxmlLoader.load();
                     Stage stage = new Stage();
-                    stage.setScene(new Scene(root1));  
+                    
+                    volumeSlider = new Slider();
+                    volumeSlider.valueProperty().addListener((observable, oldValue, newValue) -> {
+                        System.out.println("Slider Value Changed (newValue: " + newValue.intValue() + ")");
+                    });
+                    
+                    stage.setScene(new Scene(root1));                    
                     stage.show();
+                    
             } 
             catch(Exception e) {
                e.printStackTrace();
             }
+    }
+    
+    @FXML
+    private void setAudioVolume() {
+        volumeSlider.valueProperty().addListener((observable, oldValue, newValue) -> {
+            if (! volumeSlider.isValueChanging()) {
+                System.out.println("Slider Value Changed (newValue: " + newValue.intValue() + ")");
+            }
+        });
+        
     }
 
 }
