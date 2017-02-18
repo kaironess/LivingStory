abbab4c350cfd1e860cbcfcd2d1f5a89998a321f
asnakamu <amysnak@gmail.com>
2017-02-18 14:53:08 -0800

    more gameui listeners and windows

--- src/gameUI/GameController.java
+++ src/gameUI/GameController.java
@@ -14,6 +14,7 @@ import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
+import javafx.scene.control.Button;
 
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
@@ -57,22 +58,37 @@ public class GameController implements Initializable {
 
     @FXML
     private MenuItem quitGameButton;
+    
+    @FXML
+    private MenuItem closeWindowButton;
 
     @FXML
     private Menu saveMenu;
     
     // AUDIO WINDOW INJECTIONS
     @FXML
+    private Label audioTitle;
+    
+    @FXML
     private Slider volumeSlider;
 
     @FXML
-    private Label volumeLabel;
+    private Label musicVol;
+
+    @FXML
+    private Slider sfxvolumeSlider;
 
     @FXML
     private AnchorPane audioWindowBasePane;
 
     @FXML
+    private Label sfxVol;
+
+    @FXML
     private GridPane audioGrid;
+    
+    @FXML
+    private Button closeAudioButton;
 
     
     @Override // This method is called by the FXMLLoader when initialization is complete
@@ -81,12 +97,61 @@ public class GameController implements Initializable {
     }
     
     @FXML
+    private void saveGameState() {
+        System.out.println("SAVE CURR FRAME / GAME STATE");
+    }
+
+    @FXML
+    private void exitGame() {
+        System.out.println("EXIT GAME? BACK TO MAIN MENU?");
+    }
+    
+    @FXML
+    private void closeWindow() {
+        Stage stage = (Stage) myMenu.getScene().getWindow();
+        stage.close();
+    }
+    
+    @FXML
     private void openAudioDialog() {            
             try {
             FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("AudioWindow.fxml"));
                     Parent root1 = (Parent) fxmlLoader.load();
-                    Stage stage = new Stage();                    
-                    stage.setScene(new Scene(root1));                    
+                    Stage stage = new Stage();        
+                    Scene scene = new Scene(root1);
+                    stage.setScene(scene);  
+                    stage.show();
+                    
+            } 
+            catch(Exception e) {
+               e.printStackTrace();
+            }
+    }
+    
+    @FXML
+    private void openDisplayDialog() {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("DisplayWindow.fxml"));
+                    Parent root1 = (Parent) fxmlLoader.load();
+                    Stage stage = new Stage();        
+                    Scene scene = new Scene(root1);
+                    stage.setScene(scene);  
+                    stage.show();
+                    
+            } 
+            catch(Exception e) {
+               e.printStackTrace();
+            }
+    }
+    
+    @FXML
+    private void openAboutDialog() {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("AboutWindow.fxml"));
+                    Parent root1 = (Parent) fxmlLoader.load();
+                    Stage stage = new Stage();        
+                    Scene scene = new Scene(root1);
+                    stage.setScene(scene);  
                     stage.show();
                     
             } 
@@ -99,10 +164,26 @@ public class GameController implements Initializable {
     private void setAudioVolume() {
         volumeSlider.valueProperty().addListener((observable, oldValue, newValue) -> {
             if (! volumeSlider.isValueChanging()) {
-                System.out.println("Slider Value Changed (newValue: " + newValue.intValue() + ")");
+                System.out.println("Vol Slider Value Changed (newValue: " + newValue.intValue() + ")");
             }
         });
         
+    }
+    
+    @FXML
+    private void setSFXVolume() {
+        sfxvolumeSlider.valueProperty().addListener((observable, oldValue, newValue) -> {
+            if (! sfxvolumeSlider.isValueChanging()) {
+                System.out.println("SFX Slider Value Changed (newValue: " + newValue.intValue() + ")");
+            }
+        });
+
+    }
+    
+    @FXML
+    private void closeAudioWindow() {
+        Stage stage = (Stage) closeAudioButton.getScene().getWindow();
+        stage.close();
     }
 
 }
